d3b709a7e0096d37465dbcba54fcf0e9054cb30b
Aleksej Ignatenko <ignatenkoaleksej771@gmail.com>
2024-06-26 18:47:21 +0300

    Добавлены unit тесты для QuestionRepository.

---
+++ DrivingSchool.MockData/Repositories/QuestionRepositoryMock.cs
@@ -0,0 +1,127 @@
+﻿using DrivingSchool.Core.Models;
+using DrivingSchool.DataAccess.Entities;
+
+namespace DrivingSchool.MockData.Repositories
+{
+    public class QuestionRepositoryMock
+    {
+        public void Dispose()
+        {
+        }
+
+        public async Task<Guid> CreateAsync(QuestionModel question)
+        {
+            TestEntity testEntity = new TestEntity
+            {
+                Id = question.Test!.Id,
+                NameTest = question.Test.NameTest,
+            };
+
+            QuestionEntity questionEntity = new QuestionEntity
+            {
+                Id = question.Id,
+                QuestionText = question.QuestionText,
+                LinkPhoto = question.LinkPhoto,
+                Answer1 = question.Answer1,
+                Answer2 = question.Answer2,
+                Answer3 = question.Answer3,
+                Answer4 = question.Answer4,
+                CorrectAnswer = question.CorrectAnswer,
+            };
+
+            return await Task.FromResult(questionEntity.Id);
+        }
+
+        public async Task<List<QuestionModel>> GetAsync()
+        {
+            var testEntity = new TestEntity
+            {
+                Id = Guid.NewGuid(),
+                NameTest = "Test category",
+            };
+
+            var questionEntity = new List<QuestionEntity>
+            {
+                new QuestionEntity
+                {
+                    Id = Guid.NewGuid(),
+                    Test = testEntity,
+                    QuestionText = "Test",
+                    LinkPhoto = "",
+                    Answer1 = "Answer1",
+                    Answer2 = "Answer2",
+                    Answer3 = "Answer3",
+                    Answer4 = "Answer4",
+                    CorrectAnswer = "CorrectAnswer"
+                }
+            };
+
+            questionEntity[0].Test = testEntity;
+
+            var question = questionEntity
+                .Select(q => QuestionModel.Create(q.Id,
+                                TestModel.Create(q.Test.Id, q.Test.NameTest).test,
+                                q.QuestionText,
+                                q.LinkPhoto,
+                                q.Answer1,
+                                q.Answer2,
+                                q.Answer3,
+                                q.Answer4,
+                                q.CorrectAnswer).question)
+                .ToList();
+
+            return await Task.FromResult(question);
+        }
+
+        public async Task<List<QuestionModel>> GetByIdAsync(Guid id)
+        {
+            var testEntity = new TestEntity
+            {
+                Id = Guid.NewGuid(),
+                NameTest = "Test category",
+            };
+
+            var questionEntity = new List<QuestionEntity>
+            {
+                new QuestionEntity
+                {
+                    Id = Guid.Parse("64dc0c4f-74fc-483c-8fa3-7daa1b3264e7"),
+                    Test = testEntity,
+                    QuestionText = "Test",
+                    LinkPhoto = "",
+                    Answer1 = "Answer1",
+                    Answer2 = "Answer2",
+                    Answer3 = "Answer3",
+                    Answer4 = "Answer4",
+                    CorrectAnswer = "CorrectAnswer"
+                }
+            };
+
+            questionEntity[0].Test = testEntity;
+
+            var question = questionEntity
+                .Select(q => QuestionModel.Create(q.Id,
+                                TestModel.Create(q.Test.Id, q.Test.NameTest).test,
+                                q.QuestionText,
+                                q.LinkPhoto,
+                                q.Answer1,
+                                q.Answer2,
+                                q.Answer3,
+                                q.Answer4,
+                                q.CorrectAnswer).question)
+                .ToList();
+
+            return await Task.FromResult(question);
+        }
+
+        public async Task<Guid> UpdateAsync(Guid idQuestion, Guid idTest, string questionText, string linkPhoto, string answer1, string answer2, string answer3, string answer4, string correctAnswer)
+        {
+            return await Task.FromResult(idQuestion);
+        }
+
+        public async Task<Guid> DeleteAsync(Guid id)
+        {
+            return await Task.FromResult(id);
+        }
+    }
+}
---
+++ DrivingSchool.UnitTests/Repository/QuestionRepositoryTest.cs
@@ -0,0 +1,93 @@
+﻿using DrivingSchool.Core.Models;
+using DrivingSchool.MockData.Repositories;
+using JetBrains.dotMemoryUnit;
+
+namespace DrivingSchool.UnitTests.Repository
+{
+    public class QuestionRepositoryTest
+    {
+        private readonly QuestionRepositoryMock _repository;
+
+        public QuestionRepositoryTest()
+        {
+            _repository = new QuestionRepositoryMock();
+        }
+
+        [DotMemoryUnit(FailIfRunWithoutSupport = false)]
+        [Fact]
+        public async Task CreateAsync()
+        {
+            TestModel test = TestModel.Create(Guid.NewGuid(), "Test").test;
+            // Arrange
+            QuestionModel question = QuestionModel.Create(Guid.NewGuid(), test, "Text", "", "answer1", "answer2", "answer3", "answer4", "correctAnswer").question;
+
+            // Act
+            var id = await _repository.CreateAsync(question);
+
+            // Assert
+            Assert.Equal(question.Id, id);
+        }
+
+        [DotMemoryUnit(FailIfRunWithoutSupport = false)]
+        [Fact]
+        public async Task GetAsync()
+        {
+            // Arrange
+
+            // Act
+            var question = await _repository.GetAsync();
+
+            // Assert
+            Assert.Single(question);
+        }
+
+        [DotMemoryUnit(FailIfRunWithoutSupport = false)]
+        [Fact]
+        public async Task GetByIdAsync()
+        {
+            // Arrange
+            Guid id = Guid.Parse("64dc0c4f-74fc-483c-8fa3-7daa1b3264e7");
+            // Act
+            var question = await _repository.GetByIdAsync(id);
+
+            // Assert
+            Assert.Single(question);
+        }
+
+        [DotMemoryUnit(FailIfRunWithoutSupport = false)]
+        [Fact]
+        public async Task UpdateAsync()
+        {
+            // Arrange
+            Guid questionId = Guid.Parse("d833d875-660f-4d6b-a138-796a6ae98095");
+            Guid testId = Guid.Parse("5f51c061-b5f8-4579-928e-3858cd3013f5");
+            string textQuestion = "update";
+            string linkPhoto = "update";
+            string answer1 = "update";
+            string answer2 = "update";
+            string answer3 = "update";
+            string answer4 = "update";
+            string correctAnswer = "update";
+
+            // Act
+            var id = await _repository.UpdateAsync(questionId, testId, textQuestion, linkPhoto, answer1, answer2, answer3, answer4, correctAnswer);
+
+            // Assert
+            Assert.Equal(questionId, id);
+        }
+
+        [DotMemoryUnit(FailIfRunWithoutSupport = false)]
+        [Fact]
+        public async Task DeleteAsync()
+        {
+            // Arrange
+            Guid questionid = Guid.Parse("d833d875-660f-4d6b-a138-796a6ae98095");
+
+            // Act
+            var id = await _repository.DeleteAsync(questionid);
+
+            // Assert
+            Assert.Equal(questionid, id);
+        }
+    }
+}
